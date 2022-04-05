ff8c5c2290a62e474781eb6bd18b97e8ec709022
digregorio <grehgcl@hotmail.com>
2022-04-05 11:42:57 -0300

    altereando rortas

--- api/rotas/fornecedores/produtos/Produto.js
+++ api/rotas/fornecedores/produtos/Produto.js
@@ -10,7 +10,17 @@ class Produto{
         this.dataAtualizacao = dataAtualizacao
         this.versao = versao
     }
+
+    validar(){
+        if (typeof this.titulo !== 'string' || this.titulo.length === 0){
+            throw new Error('Titulo não informado')
+        }
+        if (typeof this.preco !== 'number' || this.preco <= 0){
+            throw new Error('Preço inválido')
+        }
+    }
     async criar (){
+        this.validar()
         const resultado = await Tabela.inserir({
             titulo: this.titulo,
             preco: this.preco,
@@ -24,6 +34,54 @@ class Produto{
         this.dataAtualizacao = resultado.dataAtualizacao
         this.versao = resultado.versao
 
+    }
+
+    apagar(){
+        return Tabela.remover(this.id, this.fornecedor)
+    }
+    async carregar(){
+        const produto = await Tabela.pegarPorId(this.id, this.fornecedor)
+        this.titulo = produto.titulo
+        this.preco = produto.preco
+        this.estoque = produto.estoque
+        this.dataCriacao = produto.dataCriacao
+        this.dataAtualizacao = produto.dataAtualizacao
+        this.versao = produto.versao
+    }
+    atualizar (){
+        const dadosParaAtualizar = {}
+
+        if (typeof this.titulo === 'string' && this.titulo.length > 0){
+            dadosParaAtualizar.titulo = this.titulo
+        }
+
+        if (typeof this.preco === 'number' && this.preco > 0){
+            dadosParaAtualizar.preco = this.preco
+        }
+
+        if (typeof this.estoque === 'number'){
+            dadosParaAtualizar.estoque = this.estoque
+        }
+
+        if ( Object.keys(dadosParaAtualizar).length === 0){
+            throw new Error('Nenhum dado informado para atualizar')
+        }
+        return Tabela.atualizar({
+            id: this.id,
+            fornecedor: this.fornecedor
+        },
+        dadosParaAtualizar
+        )
+
+        
+    }
+    diminuirEstoque(quantidade){
+        return Tabela.subtrair(
+            this.id,
+            this.fornecedor,
+            'estoque',
+            this.estoque
+        )
     }
 }
 
--- api/rotas/fornecedores/produtos/TabelaProduto.js
+++ api/rotas/fornecedores/produtos/TabelaProduto.js
@@ -1,15 +1,60 @@
 const Modelo = require('./ModeloTabelaProduto')
+const instancia = require('../../../banco-de-dados')
 
 module.exports = {
     listar(idFornecedor){
         return Modelo.findAll({
             where: {
                 fornecedor: idFornecedor
-            }
+            },
+            raw: true
         })
     },
     inserir (dados) {
         return Modelo.create(dados)
+    },
+    remover(idProduto, idFornecedor){
+        return Modelo.destroy({
+            where: {
+                id: idProduto,
+                fornecedor: idFornecedor
+            },
+            raw: true
+        })
+    },
+    async pegarPorId(idProduto, idFornecedor){
+        const encontrado = await Modelo.findOne({
+            where: {
+                id: idProduto,
+                fornecedor: idFornecedor
+            }
+    })
+    if (!encontrado){
+        throw new Error('Produto não encontrado')
     }
-    
-}
+    return encontrado
+},
+atualizar (dadosDoProduto, dadosParaAtualizar){
+    return Modelo.update(dadosParaAtualizar, 
+        {
+        where:dadosDoProduto 
+        }
+ 
+        )
+    },
+    subtrair (idProduto, idFornecedor, quantidade){
+        return instancia.transaction(async (transacao) => {
+            const produto= await Modelo.findOne({
+                where: {
+                    id: idProduto,
+                    fornecedor: idFornecedor
+                }
+            })
+            produto[campo] = quantidade
+
+            await produto.save({transaction: transacao})
+
+            return produto
+        })
+    },
+    }
